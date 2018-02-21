d4517aa913be582d2971764ee62a20288fa954dc
Bhagvat Mehra <mehra_b@India.XoriantCorp.com>
2018-02-21 17:28:16 +0530

    Until Week 2 completed

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-/* created list of articles data that varies from one page to other
+/* NA as created list of articles data that varies from one page to other
 var articleOne={
 	title: 'Article One | Bhagvat',
 	heading: 'Article One',
 	date: 'Sep 5, 2016',
 	content:`
 	<p>
-					This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.
+			This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.
 	</p>`
 };
 */
@@ -24,10 +24,9 @@ var articles={
 		date: 'Sep 5, 2016',
 		content:`
 		<p>
-						This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.
+			This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.This is the content from first article.
 		</p>`
-	},
-	
+	},	
 	'article-two': {
 		title: 'Article Two | Bhagsi',
 		heading: 'Article Two',
@@ -37,14 +36,14 @@ var articles={
 						This is the content from Second article.
 		</p>`
 	}	
-}
+};
 
 // function to create common template (for article-one and article-two) and the dynamic data is filled by the calling function
 function createTemplate (data){
-	var title=data.title;
-	var heading=data.heading;	
-	var date=data.date;
-	var content=data.content;
+	var title = data.title;
+	var heading = data.heading;	
+	var date = data.date;
+	var content = data.content;
 
 	var htmlTemplate=`
 	<html>
@@ -70,14 +69,43 @@ function createTemplate (data){
 				</div>
 			</div>
 		</body>
-	</html>`
-	
+	</html>
+	`;	
 	return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
+
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+  // Get the name from the request
+  var name= req.params.name;
+  names.push(name);
+  
+  //JSON - javascript object notation - to covert javascript object to string
+  res.send(JSON.stringify(names));
+});
+
+//above function implementation now using query parameter string (Also this function I have placed before "app.get('/:articleName', function (req, res)" to avoid error that might result into parameters not being handled)
+app.get('/submit-name', function (req, res) {   //URL: /submit-name?name=xxx
+  // Get the name from the request
+  var name= req.query.name;
+  names.push(name);
+  
+  //JSON - javascript object notation - to covert javascript object to string
+  res.send(JSON.stringify(names));
+});
+
+
 
 /*
 app.get('/article-one', function (req, res) {
@@ -93,27 +121,29 @@ app.get('/article-two', function (req, res) {
 
 app.get('/:articleName', function (req, res) {    // /:articleName  - provided by express framework
 	var articleName= req.params.articleName;
-  res.send(createTemplate(articles[articleName]));  // new implementation wherein content is published dynamically at run time
+	res.send(createTemplate(articles[articleName]));  // new implementation wherein content is published dynamically at run time
 });
 
+/*
+//Article three in current form will not work as request will go to above function validation and will not reach out to this function
 app.get('/article-three', function (req, res) {
   res.send("This is from Article-Three page");
 });
-
-
-var counter=0;
-app.get('/counter',function(req,res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {                  // To handle javascript file 'ui/main.js' (dealing with client)
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,92 @@
+console.log('Loaded!!!');
+
+//Change the text of the div element where id='main-txt'
+var element= document.getElementById('main-txt');
+element.innerHTML='New value';
+
+/*
+// move the image by 200 pixel on click
+var img=document.getElementById('madi');
+img.onclick	= function(){
+	img.style.marginLeft='200px';
+};
+*/
+
+//move the image gradually like animation
+var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+	marginLeft = marginLeft+1;
+	img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick	= function(){
+	var interval= setInterval(moveRight,100);
+};
+
+
+
 //counter code
 var button=document.getElementById('counter');
 var counter=0;
 
-button.onClick= function(){
-    //Make a request to counter endpoint
+button.onclick= function(){
+    //Crete a request object
+    var request = new XMLHttpRequest();
+	
+	// capture the response and store it in a variable
+	request.onreadystatechange=function(){
+		if(request.readyState==XMLHttpRequest.DONE){
+			if(request.status==200){
+				//code for counter increment
+				var counter=request.responseText;
+				var span= document.getElementById('count');
+				span.innerHTML=counter.toString();      
+			}	
+		}
+	};
     
-    // capture the response and store it in a variable
-    
-    //Render the variable in correct span
+	//Make a request to counter endpoint
+	request.open("GET",'http://localhost:8080/counter',true);
+	request.send(null);
+	
+    /*Manual: Render the variable in correct span
+	var span= document.getElementById('count');
+    span.innerHTML=counter.toString();      
     counter=counter + 1;
-    var span= document.getElementById('count');
-    span.innerHTML=counter.toString();
-        
+    */
 };
+
+
+//Submit name logic
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){       // Make a request to server and send the name
+	//Crete a request object
+    var request = new XMLHttpRequest();
+	
+	// capture the response and store it in a variable
+	request.onreadystatechange=function(){
+		if(request.readyState==XMLHttpRequest.DONE){
+			if(request.status==200){
+				// Capture a list of names and render it as a list
+				var names=request.responseText;
+				names=JSON.parse(names);   // Reverse what happen at server side: convert a string into array elements
+				var list='';
+				for(var i=0;i<names.length;i++){
+					list+='<li>'+ names[i] +'</li>';
+				}
+				var ul= document.getElementById('namelist');
+				ul.innerHTML = list;
+			}	
+		}
+	};
+    
+	//Make a request to 'submit-name' endpoint
+	//var nameInput=document.getElementById('name');
+	//var name=nameInput.value;
+	var name=document.getElementById('name').value;
+	request.open("GET",'http://localhost:8080/submit-name?name='+name,true);
+	request.send(null);
+
+};
+
+
