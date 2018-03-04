fc1aeddf9d893205600acc525622bbbd14fa6d94
bhagvatatwork <36442586+bhagvatatwork@users.noreply.github.com>
2018-03-04 13:56:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,6 +96,22 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
+app.get('/articles/:articleName', function (req, res) {    // :articleName  - provided by express framework
+	pool.query("SELECT * FROM article WHERE title = '", + req.params.articleName + "'",function(err,result){
+	   if(err) {
+	       res.status(500).send(err.toString());
+	   }else{
+	       if(result.rows.length === 0){
+	           res.status(404).send("Article not found...");
+	       }
+	       else{
+	           var articleData=result.rows[0];
+	           res.send(createTemplate(articleData));
+	       }
+	   }
+	});
+});
+
 
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
@@ -116,25 +132,6 @@ app.get('/submit-name', function (req, res) {   //URL: /submit-name?name=xxx
   //JSON - javascript object notation - to covert javascript object to string
   res.send(JSON.stringify(names));
 });
-
-
-app.get('/articles/:articleName', function (req, res) {    // :articleName  - provided by express framework
-	
-	pool.query("SELECT * FROM article WHERE title = '", + req.params.articleName + "'",function(err,result){
-	   if(err) {
-	       res.status(500).send(err.toString());
-	   }else{
-	       if(result.rows.length === 0){
-	           res.status(404).send("Article not found...");
-	       }
-	       else{
-	           var articleData=result.rows[0];
-	           res.send(createTemplate(articleData));
-	       }
-	   }
-	});
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
