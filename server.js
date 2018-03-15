a26c0f6eab1b1c0327ac36560fed77d577960c05
bhagvatatwork <36442586+bhagvatatwork@users.noreply.github.com>
2018-03-15 10:41:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     // How do we create a hash?
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha-512');
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     console.log('hashed password value (HPV): ',hashed);
     console.log('HPV (encoded in hex format): ',hashed.toString('hex'));
     return hashed.toString('hex');
