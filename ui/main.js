f06330a5a748199acfb3856ddd67cd059e801980
bhagvatatwork <36442586+bhagvatatwork@users.noreply.github.com>
2018-03-15 14:00:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,64 +1,8 @@
 console.log('Loaded!!!');
 
-//Change the text of the div element where id='main-txt'
-var element= document.getElementById('main-txt');
-element.innerHTML='New value';
-
-/*
-// move the image by 200 pixel on click
-var img=document.getElementById('madi');
-img.onclick	= function(){
-	img.style.marginLeft='200px';
-};
-*/
-
-//move the image gradually like animation
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-	marginLeft = marginLeft+1;
-	img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick	= function(){
-	var interval= setInterval(moveRight,100);
-};
-
-
-
-//counter code
-var button=document.getElementById('counter');
-var counter=0;
-
-button.onclick= function(){
-    //Crete a request object
-    var request = new XMLHttpRequest();
-	
-	// capture the response and store it in a variable
-	request.onreadystatechange=function(){
-		if(request.readyState==XMLHttpRequest.DONE){
-			if(request.status==200){
-				//code for counter increment
-				var counter=request.responseText;
-				var span= document.getElementById('count');
-				span.innerHTML=counter.toString();      
-			}	
-		}
-	};
-    
-	//Make a request to counter endpoint
-	request.open("GET",'http://bhagvatatwork.imad.hasura-app.io/counter',true);
-	request.send(null);
-	
-    /*Manual: Render the variable in correct span
-	var span= document.getElementById('count');
-    span.innerHTML=counter.toString();      
-    counter=counter + 1;
-    */
-};
-
-
-//Submit name logic
+//Submit username/pwd to login
 var submit = document.getElementById('submit_btn');
+
 submit.onclick=function(){       // Make a request to server and send the name
 	//Crete a request object
     var request = new XMLHttpRequest();
@@ -67,26 +11,24 @@ submit.onclick=function(){       // Make a request to server and send the name
 	request.onreadystatechange=function(){
 		if(request.readyState==XMLHttpRequest.DONE){
 			if(request.status==200){
-				// Capture a list of names and render it as a list
-				var names=request.responseText;
-				names=JSON.parse(names);   // Reverse what happen at server side: convert a string into array elements
-				var list='';
-				for(var i=0;i<names.length;i++){
-					list+='<li>'+ names[i] +'</li>';
-				}
-				var ul= document.getElementById('namelist');
-				ul.innerHTML = list;
+			    alert('Logged in successfully');
+			}else if(request.status ===403){
+			    alert('username/password is invalid');
+			}else if(request.status ===500){
+			    alert('something went wrong on the server');
 			}	
 		}
 	};
     
-	//Make a request to 'submit-name' endpoint
-	//var nameInput=document.getElementById('name');
-	//var name=nameInput.value;
-	var name=document.getElementById('name').value;
-	request.open("GET",'http://bhagvatatwork.imad.hasura-app.io/submit-name?name='+name,true);
-	request.send(null);
-
+	//Make a request 
+	var username=document.getElementById('username').value;
+	var password=document.getElementById('password').value;
+	console.log(username);
+	console.log(password);
+	
+	request.open("POST",'http://bhagvatatwork.imad.hasura-app.io/login',true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.strigify({username: username, password: password}));
 };
 
 
